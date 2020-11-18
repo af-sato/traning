c377c9065b51ac934846cddfc084cd2c890bb78c
m.sato <m.sato@a-force.co.jp>
2020-11-18 13:49:10 +0900

    ユーザーID重複機能修正（実装中）

--- membersite/src/main/java/com/example/membersite/controller/UserController.java
+++ membersite/src/main/java/com/example/membersite/controller/UserController.java
@@ -6,15 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.membersite.model.form.UserForm;
 import com.example.membersite.model.mapper.UserMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class UserController {
@@ -25,6 +27,16 @@ public class UserController {
 	@RequestMapping("/membersite/user")
 	public String index() {
 		return "register_user";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/membersite/checkId", method = RequestMethod.POST)
+	public boolean checkId(@RequestBody String json) throws JsonMappingException, JsonProcessingException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode jsonNode = objectMapper.readTree(json);
+		String id = jsonNode.get("id").textValue();
+		int count = userMapper.findById(id);
+		return count > 0;
 	}
 	
 	@RequestMapping(value = "/membersite/register", method = RequestMethod.POST)
@@ -56,12 +68,5 @@ public class UserController {
 			//TODO アラート
 			return "register_user";
 		}
-	}
-	
-	@ResponseBody
-	@RequestMapping("/membersite/checkId")
-	public boolean checkId(@RequestBody String ID) {
-		int count = userMapper.findById(ID);
-		return count > 0;
 	}
 }
